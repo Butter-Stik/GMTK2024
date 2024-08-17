b337e7716a9b7f5e30789b3da066c3068be40afa
pantaelaman <calvin@theophoffs.com>
2024-08-17 19:14:53 -0400

    added window dragging

--- Scripts/window.gd
+++ Scripts/window.gd
@@ -2,8 +2,6 @@ extends Node2D
 
 @export var SHAPE: RectangleShape2D;
 var dragging := Vector2.ZERO;
-var relative_to: Vector2;
-var cumulative_delta := Vector2.ZERO;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,39 +20,25 @@ func update_size():
 		child.emit_signal("resized");
 	
 
-func update_region_collision_box(region_path: StringName):
-	var region := get_node(\
-		"Area/Collision/NinePatchRect/" + region_path) as Control;
-	var area := region.get_child(0) as Area2D;
-	var collision := area.get_child(0) as CollisionShape2D;
-	var shape := collision.shape if collision.shape != null else RectangleShape2D.new();
-	shape.size = region.size;
-	collision.shape = shape;
-	collision.position = collision.shape.size / 2;
-	
-
-func _unhandled_input(event):
-	if dragging != Vector2.ZERO and event is InputEventMouse:
-		var ignore_axes: Vector2 = dragging.abs();
-		var local_mouse_position: Vector2 = $Area.get_local_mouse_position();
-		var mouse_position: Vector2 = \
-			local_mouse_position * \
-			ignore_axes;
-		var delta: Vector2 = (mouse_position - relative_to).snapped(Vector2.ONE);
-		$Area.position -= delta - cumulative_delta;
-		SHAPE.size += delta - cumulative_delta;
-		cumulative_delta += delta - cumulative_delta;
+func _input(event):
+	if dragging != Vector2.ZERO and event is InputEventMouseMotion:
+		var mouse_delta := ((event as InputEventMouseMotion).relative * \
+			dragging.abs() / \
+			get_viewport().get_camera_2d().zoom) \
+			.snapped(Vector2.ONE); # ignore 0.0 directions
+		SHAPE.size += mouse_delta * dragging;
+		SHAPE.size = SHAPE.size.abs().max(Vector2.ONE * 24.0) * SHAPE.size.sign();
+		var delta = $Area/Collision/NinePatchRect.size - SHAPE.size;
+		print(delta);
+		$Area.position -= delta * dragging / 2;
 		update_size();
-	if event is not InputEventMouseButton:
-		return
-	if !(event as InputEventMouseButton).pressed:
+		
+	elif event is InputEventMouseButton and \
+		!(event as InputEventMouseButton).pressed:
 		dragging = Vector2.ZERO;
 	
 
-func potential_resize_starting(viewport: Node, event: InputEvent, shape_idx: int, direction: Vector2):
-	if event is not InputEventMouseButton:
-		return
-	if (event as InputEventMouseButton).pressed:
-		dragging = direction
-		relative_to = $Area.get_local_mouse_position();
+func region_input(event: InputEvent, direction: Vector2):
+	if event is InputEventMouseButton and (event as InputEventMouseButton).pressed:
+		dragging = direction;
 	
