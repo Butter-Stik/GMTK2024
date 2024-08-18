7107e73e18a5c26c498324815b183a99ac25b19d
FyNnNiL <135078863+FyNnNiL@users.noreply.github.com>
2024-08-18 11:53:09 -0400

    Update box.gd

--- Scenes/box.gd
+++ Scenes/box.gd
@@ -1,5 +1,13 @@
 extends RigidBody2D
-class_name Box
+class_name Pushbox
+
+@export var DESTROY: bool = false:
+	set(new_destroy):
+		DESTROY = new_destroy;
+		if new_destroy:
+			$Powerable.TYPE = Constants.PowerableType.DESTROY;
+		else:
+			$Powerable.TYPE = Constants.PowerableType.NONE;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,8 +16,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$AnimatedSprite2D.play("default")
-	pass
+	if DESTROY == false:
+		$AnimatedSprite2D.play("non_breakable")
+	elif DESTROY == true:
+		$AnimatedSprite2D.play("breakable")
+
 
 func die():
 	queue_free();
