076059255cef11819700f53b2f3a93e34d7d9472
pantaelaman <calvin@theophoffs.com>
2024-08-18 17:36:44 -0400

    Updated powerable to trigger appropriately on load

--- Scripts/gate.gd
+++ Scripts/gate.gd
@@ -1,30 +1,29 @@
 extends StaticBody2D
 
-var state: bool = false;
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if state:
-		get_node("CollisionShape2D").disabled = true
-	else:
-		get_node("CollisionShape2D").disabled = false
-		
-	if $Powerable.power_state == Constants.Power.ON:
-		if state:
-			$AnimatedSprite2D.play("open_on")
+var state: bool = false:
+	set(new_state):
+		if new_state == state:
+			return
+		state = new_state;
+		if new_state:
+			set_collision_layer_value(1, false);
+			$AnimatedSprite2D.play(\
+				"open_on" \
+				if $Powerable.power_state == Constants.Power.ON \
+				else "open_off");
 		else:
-			$AnimatedSprite2D.play("closed_on")
-	else:
-		if state:
-			$AnimatedSprite2D.play("open_off")
-		else:
-			$AnimatedSprite2D.play("closed_off")
+			set_collision_layer_value(1, true);
+			$AnimatedSprite2D.play(\
+				"closed_on" \
+				if $Powerable.power_state == Constants.Power.ON \
+				else "closed_off");
 
 func on_trigger_state_changed(new_state: bool):
 	print(new_state);
 	state = new_state;
+
+func _on_power_changed(power):
+	if power == Constants.Power.ON:
+		$AnimatedSprite2D.play("open_on" if state else "closed_on");
+	else:
+		$AnimatedSprite2D.play("open_off" if state else "closed_off");
