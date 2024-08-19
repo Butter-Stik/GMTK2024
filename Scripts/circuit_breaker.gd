4aaef601b9bf78bfe2115380e274130ead254ba4
FyNnNiL <135078863+FyNnNiL@users.noreply.github.com>
2024-08-18 23:04:30 -0400

    added usability to circuits

--- Scripts/circuit_breaker.gd
+++ Scripts/circuit_breaker.gd
@@ -24,3 +24,4 @@ func _on_body_entered(body: Node2D) -> void:
 	pulled = true;
 	if pulled == true:
 		$AnimatedSprite2D.play("pulled")
+		get_tree().change_scene_to_file("res://Scenes/sample_level.tscn")
