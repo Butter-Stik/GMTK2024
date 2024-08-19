9951a65fb70195df026c1edce013bb4d481a17ae
FyNnNiL <135078863+FyNnNiL@users.noreply.github.com>
2024-08-18 21:01:01 -0400

    animated circuit breaker (not functional yet)

---
+++ Scripts/circuit_breaker.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+var pulled: bool = false;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_powerable_power_changed(power: Constants.Power) -> void:
+	if pulled == false:
+		if $Powerable.power_state == Constants.Power.ON:
+			$AnimatedSprite2D.play("not_pulled_on")
+		else:
+			$AnimatedSprite2D.play("not_pulled_off")
+
+
+func _on_body_entered(body: Node2D) -> void:
+	pulled = true;
+	if pulled == true:
+		$AnimatedSprite2D.play("pulled")
