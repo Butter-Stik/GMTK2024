4ba850ffc8174f035c3521bc1a6324eee11884f0
pantaelaman <calvin@theophoffs.com>
2024-08-19 22:25:26 -0400

    Adjusted button size and structure

--- Scripts/level_menu.gd
+++ Scripts/level_menu.gd
@@ -5,6 +5,10 @@ func _ready():
 	for i in min($"/root/Death".LEVELS_COMPLETED + 1, $GridContainer.get_child_count()):
 		$GridContainer.get_child(i).set_disabled(false);
 
-func _on_button_pressed():
+func _on_reset_pressed():
 	$"/root/Death".LEVELS_COMPLETED = 0;
 	get_tree().reload_current_scene();
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn");
